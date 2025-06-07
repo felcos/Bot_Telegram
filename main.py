b40ef75c19b0357ba39e001046e1a1abd18e70b9
Felipe Costales <felcos@gmail.com>
2025-06-07 14:34:07 +0000

    Main actualizado con control de errores y detección de plantillas

--- main.py
+++ main.py
@@ -72,4 +72,9 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, responder))
-    app.run_polling()
+    app.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.environ.get("PORT", 8443)),
+    webhook_url="https://bot-telegram-yzpk.onrender.com"
+)
+
